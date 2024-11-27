cb1aacf8459be38561c09d42f9d029800b6d6463
Tianao <tianao.jia@ganjing.dev>
2024-11-27 11:37:13 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Adelaide Jia on 2024/11/27.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
